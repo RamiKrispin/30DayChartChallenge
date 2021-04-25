520d6117ae927789888f0ef34bfb8b79b9a61bc2
RamiKrispin <ramkrisp@umich.edu>
2021-04-25 13:39:13 -0700

    update the code

--- day22/animation.R
+++ day22/animation.R
@@ -62,10 +62,27 @@ md <- train_model(input = ts.obj,
 plot_error(md)
 
 plot_model(md) %>%
-  layout(title = "Time Series Models Horse Racing with Backtesting",
-         xaxis = list(range = c(2016, 2020.5)),
-         yaxis = list(title = "Number of Passengers"),
+  layout(title = paste("The US Monthly Demand for Natural Gas",
+                       "Training Mutliple Forecasting Models with Backtesting",
+                       sep = "<br>"),
+         xaxis = list(title = "",
+                      range = c(2016, 2020.5)),
+         yaxis = list(title = "Million Cubic Feet",
+                      range = c(2.9 * 10 ^ 6, 6 * 10 ^ 6)),
          plot_bgcolor = "black",
          paper_bgcolor = "black",
          font = list(color = "white"),
-         xaxis = list(zerolinecolor = "white"))
+         xaxis = list(zerolinecolor = "white"),
+         margin = list(t = 50, b = 150, l = 80, r = 30))  %>%
+  add_annotations(text = paste("#30DayChartChallenge Day 22",
+                               "Viz: Rami Krispin",
+                               "Data: Energy Information Administration (EIA)",
+                               sep = " | "),
+                  font = list(size = 13,
+                              family = "Ariel"),
+                  align = "left",
+                  x = -0.05,
+                  y = -0.46,
+                  xref = "paper",
+                  yref = "paper",
+                  showarrow = FALSE)
